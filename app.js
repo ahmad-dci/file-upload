b9d2426e40a2afabad2dcd029c4939ea18d17adb
ahmad-dci <ahmad.osman@digitalcareerinstitute.org>
2019-05-21 23:49:53 +0200

    finish

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express();
 // default options
 app.use(fileUpload());
 
+
+
 app.post('/upload', function(req, res) {
   if (Object.keys(req.files).length == 0) {
     return res.status(400).send('No files were uploaded.');
@@ -14,10 +16,14 @@ app.post('/upload', function(req, res) {
   let sampleFile = req.files.sampleFile;
 
   // Use the mv() method to place the file somewhere on your server
-  sampleFile.mv('/somewhere/on/your/server/filename.jpg', function(err) {
+  sampleFile.mv(__dirname+'/upload/filename.jpg', function(err) {
     if (err)
       return res.status(500).send(err);
 
     res.send('File uploaded!');
   });
+})
+app.get('/',(req, res)=>{
+    res.sendFile(__dirname+'/public/index.html');
 });
+app.listen(3000)
